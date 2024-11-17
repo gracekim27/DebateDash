f1330b67029f508e7491fe783b540baef1c0980d
gracekim27 <gracejkim9@gmail.com>
2024-11-17 15:16:31 -0500

    firstBar size update

--- js/firstBar.js
+++ js/firstBar.js
@@ -13,8 +13,8 @@ class FirstBar {
 
         // Define margins and dimensions based on the parent element's size
         vis.margin = {top: 60, right: 20, bottom: 100, left: 50}; // Increased top margin for title
-        vis.width = (document.getElementById(vis.parentElement).getBoundingClientRect().width/2) - vis.margin.left - vis.margin.right;
-        vis.height = (document.getElementById(vis.parentElement).getBoundingClientRect().width / 2) - vis.margin.top - vis.margin.bottom;
+        vis.width = (document.getElementById(vis.parentElement).getBoundingClientRect().width/3) - vis.margin.left - vis.margin.right;
+        vis.height = (document.getElementById(vis.parentElement).getBoundingClientRect().width / 3) - vis.margin.top - vis.margin.bottom;
 
         // SVG drawing area
         vis.svg = d3.select(`#${vis.parentElement}`)
